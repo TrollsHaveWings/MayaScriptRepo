35f49729c6bdbb89eb4bdc2b93a2e4e2d295eb25
Noah Manson <noah.manson@protonmail.com>
2024-02-06 19:38:56 +0000

    Created Flock of Boids
    Created the flock of boids, on initialisation they are randomly created at different locations and rotations.
    
    There is an expression that moves the boids forward on there OBJECT SPACE Y axis

---
+++ boids.py
@@ -0,0 +1,40 @@
+import maya.cmds as cmds
+import random
+import math
+
+master_boid_name = "boid"
+boid_size = 10
+boid_count = 100
+boid_spawn_radius = 1000
+velocity = 200  # velocity along Y-axis
+
+main_group_name = "FLOCK_group"
+
+# Create the main group if it doesn't exist
+if not cmds.objExists(main_group_name):
+    cmds.group(empty=True, name=main_group_name)
+
+# Create boids
+for i in range(0, boid_count):
+    xSpawn = random.uniform(-1 * boid_spawn_radius, boid_spawn_radius)
+    ySpawn = random.uniform(-1 * boid_spawn_radius, boid_spawn_radius)
+    zSpawn = random.uniform(-1 * boid_spawn_radius, boid_spawn_radius)
+    
+    boid_name = master_boid_name + str(i)  # Create a unique name for each boid
+
+    # Use cone GEO to represent boid
+    cmds.polyCone(name = boid_name, radius = 1*boid_size, height = 2*boid_size)
+    cmds.xform(boid_name, t=[0, 0, 0], ws=True)  # Reset the boid's position
+
+    # Create a group for the boid and move/rotate the group
+    ctrl_group_name = "CTRL_" + boid_name
+    cmds.group(boid_name, name=ctrl_group_name)
+    cmds.xform(ctrl_group_name, t=[xSpawn, ySpawn, zSpawn], ws=True)
+    cmds.xform(ctrl_group_name, ro=[xSpawn, ySpawn, zSpawn], os=True)
+
+    # Parent the CTRL_ group to the main group
+    cmds.parent(ctrl_group_name, main_group_name)
+
+    # Create an expression to animate the boid
+    expression = f"{boid_name}.translateY = {velocity} * time;"
+    cmds.expression(s=expression)
